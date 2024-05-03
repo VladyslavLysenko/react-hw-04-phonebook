532e426cfb3c265536c2243056b5a2c3b4258284
Nancy311 <105671987+VladyslavLysenko@users.noreply.github.com>
2024-05-03 09:45:29 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,14 @@ import { ContactsList } from './ContactsList';
 import { Filter } from './Filter';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const parcedContacts = JSON.parse(savedContacts);
+      return parcedContacts;
+    }
+    return [];
+  });
 
   const [filter, setFilter] = useState('');
 
@@ -52,7 +54,6 @@ export const App = () => {
         },
       ]);
 
-      
       // console.log('checkName', checkName);
       // console.log(contacts);
 
